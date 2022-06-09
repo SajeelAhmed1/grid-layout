2bed3cd631b1be952da40ec339d65acd0a605ceb
Sajeel Ahmed Siddiqui <sajeel.ahmed@fmsl.local>
2022-06-09 19:27:34 +0500

    grid commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import "./App.css";
+
+function App() {
+  return (
+    <div className="wrapper">
+      <div className="item">One</div>
+      <div className="item">Two</div>
+      <div className="item">Three</div>
+      <div className="item">Four</div>
+      {/* <div className="item">Five</div> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Grid.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Grid () {
+  
+  return (
+
+    <div className="container">
+      <div className="item">1</div>
+      <div className="item">2</div>
+      <div className="item">3</div>
+      <div className="item">4</div>
+      
+    </div>
+  )
+}
